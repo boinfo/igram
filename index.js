ba2d2705c3b9fd05f2fb78046a6f7c64ac9fbe56
boinfo <142826699+boinfo@users.noreply.github.com>
2024-07-09 21:14:31 +0600

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const fs = require('fs');
 const ig = require('ig-unduh')
 let axios = require("axios");
 const Youtube = require('youtube-stream-url');
-const ndown = require("imran-downloader");
 const { pinterestdl } = require('imran-servar');
 
 
@@ -25,49 +24,6 @@ app.get('/pin', async function (req, res) {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
-
-
-
-
-app.get('/insta', async function (req, res) {
-  try {
-    let link = req.query.url;
-
-    // Assuming 'ig' is a function that processes the Instagram link asynchronously
-    let result = await ig(link);
-
-    let d1 = result.data[0].url;
-    
-
-let newUrlPrefix = "https://ig95.snap-data.xyz";
-
-let indexOfIg = d1.indexOf("/ig/");
-
-let d2 = newUrlPrefix + d1.slice(indexOfIg);
-
-    res.json({ link: result.data[0].url,
-             dl: d2});
-    console.log(result.data[0].url);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-
-
-
-app.get('/insta2', async function (req, res) {
-  try {
-    let link = req.query.url;
-
-    let vdourl = await ndown(link);
-
-    res.json({ vdourl });
-  } catch (err) {
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-
 
 
 app.get('/yt', async function (req, res) {
